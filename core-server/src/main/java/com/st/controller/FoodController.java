9f73cae50aa54feae4a329586abf42b8a00b7d99
Siten616 <shentao0616@qq.com>
2021-02-01 16:09:44 +0800

    RabbitMQ

--- core-server/src/main/java/com/st/controller/FoodController.java
+++ core-server/src/main/java/com/st/controller/FoodController.java
@@ -1,10 +1,15 @@
 package com.st.controller;
 
 import com.st.domain.Food;
+import com.st.rabbitmq.springboot.RabbitSender;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @Author: shentao
@@ -14,11 +19,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FoodController {
 
-    @ResponseBody
+    @Autowired
+    RabbitSender rabbitSender;
+
     @RequestMapping(value = "/showfood", method = RequestMethod.GET)
     public Food showFood() {
         Food food = new Food(1, "苹果", "5");
         System.out.println(food);
         return food;
+    }
+
+    @RequestMapping(value = "/mq", method = RequestMethod.GET)
+    public String sendRabbitMQ() {
+        Map map = new HashMap();
+        map.put("食物", "水果");
+        try {
+            rabbitSender.send("你好啊", map);
+            return "RabbitMQ 发送成功";
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "error";
     }
 }
